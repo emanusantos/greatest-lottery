7947349e2f82dbb8fbc09cc142a34061490b87e9
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-08-09 10:16:23 -0300

    deleted index.css and created global styles with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routes from './routes';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import GlobalStyles from './globalStyles';
 
 function App() {
   return (
     <Router>
+      <GlobalStyles />
       <Routes />
     </Router>
   );
---
+++ src/globalStyles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+
+body {
+    font-family: Helvetica;
+    background-color: #F7F7F7;
+};
+
+`;
+
+export default GlobalStyles;
